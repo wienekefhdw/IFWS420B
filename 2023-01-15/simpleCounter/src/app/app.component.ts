994aac17fdff6e85c419b2ea2a71ed39fee57d3b
Matthis <github@matthiswieneke.de>
2023-01-16 11:29:30 +0100

    simplecounter

---
+++ 2023-01-15/simpleCounter/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  counter = 0;
+
+  decrement(){
+    this.counter--;
+  }
+
+  increment(){
+    this.counter++;
+  }
+}
