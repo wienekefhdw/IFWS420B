1378b2813b3d9dc28c2e676745a3a2430f4823df
kellermatthis <github@matthiswieneke.de>
2023-02-10 21:01:59 +0100

    Exercices

---
+++ 2023-02-15/JSONExample2.ts
@@ -0,0 +1,7 @@
+// Create an object of the example class and parse it into valid json
+class Example {
+    empCode: number = 0;
+    empName: string = '';
+    understoodExample: boolean = false;
+    topThreeNumbers: number[] =[];
+}
